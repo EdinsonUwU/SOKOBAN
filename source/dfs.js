c6f0d60b710ce0e782b2eb288bdd14b5a4bcccf4
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-04-20 09:05:03 -0500

    view image

--- source/dfs.js
+++ source/dfs.js
@@ -1,11 +1,10 @@
 import { apply_opperators } from "/source/aux_functions.js";
 export async function dfs(canvas, matrix, state) {
     //console.log(apply_opperators(matrix,state))
-    var new_states = apply_opperators(matrix,state)
-    console.log(new_states)
+    //var new_states = apply_opperators(matrix,state)//esto va al allar los nodos hijo
 
 
-
+    
     console.log("aa")
     //Sleep for 3 seconds
     await new Promise((r) => setTimeout(r, 1000));
